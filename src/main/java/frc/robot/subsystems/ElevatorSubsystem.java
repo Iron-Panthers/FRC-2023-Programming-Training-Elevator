4d56e5c74f79bd981db360a770070c25249062c7
msiu888 <mayalauren.siu@gmail.com>
2023-10-19 16:11:37 -0700

    Revert "Restarting"
    This reverts commit 46b15ec548b2bb615b1d0f5c589d1b6e33a98d7c.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -23,6 +23,7 @@ public class ElevatorSubsystem extends SubsystemBase {
 
   private final ShuffleboardTab ElevatorTab = Shuffleboard.getTab("Elevator");
 
+<<<<<<< HEAD
     private double targetHeight;
     private double motorPower;
 
@@ -32,6 +33,19 @@ public class ElevatorSubsystem extends SubsystemBase {
    right_motor = new TalonFX(6);
     
    right_motor.configFactoryDefault();
+=======
+  private double targetHeight;  
+
+  private double motorPower;
+
+  /** Creates a new ElevatorSubsystem. */
+  public ElevatorSubsystem() {
+
+    left_motor = new TalonFX(7);
+    right_motor = new TalonFX(6);
+   
+    right_motor.configFactoryDefault();
+>>>>>>> main
     left_motor.configFactoryDefault();
 
     right_motor.clearStickyFaults();
@@ -51,22 +65,37 @@ public class ElevatorSubsystem extends SubsystemBase {
     right_motor.setSelectedSensorPosition(0);
 
     // make sure we hold our height when we get disabled
-    right_motor.setNeutralMode(NeutralMode.Brake);
-    left_motor.setNeutralMode(NeutralMode.Brake);
+    right_motor.setNeutralMode(NeutralMode.Coast);
+    left_motor.setNeutralMode(NeutralMode.Coast);
 
-    left_motor.follow(right_motor);
+   
 
+    targetHeight = 0;
+
+    motorPower = 0;
+
+    ElevatorTab.addNumber("Current Motor Power", () -> this.motorPower);
+    ElevatorTab.addNumber("Target Height", () -> this.targetHeight);
+
+    
+    ElevatorTab.addNumber("Left Motor Speed", left_motor::getSelectedSensorVelocity);
+    ElevatorTab.addNumber("Right Motor Speed", right_motor::getSelectedSensorVelocity);
+
+<<<<<<< HEAD
     targetHeight = 0;
 
     ElevatorTab.addNumber("Current Motor Powet", () -> this.motorPower);
     ElevatorTab.addNumber("target height", () -> this.targetHeight);
 
+=======
+>>>>>>> main
     // ElevatorTab.addNumber("height", () -> this.currentHeight);
     // ElevatorTab.addNumber("target height", () -> this.targetHeight);
     // ElevatorTab.addNumber("right motor sensor value", this::getHeight);
 
   }
 
+<<<<<<< HEAD
 public void setMotorPower(double motorPower){
   this.motorPower = MathUtil.clamp(motorPower, 0d, 0.25);
   
@@ -76,6 +105,22 @@ public void setMotorPower(double motorPower){
   public void periodic() {
     right_motor.set(TalonFXControlMode.PercentOutput, motorPower);
 
+=======
+  public void setMotorPower(double motorPower){
+    this.motorPower = MathUtil.clamp(motorPower, -0.25, 0.25);
+  }
+
+  @Override
+  public void periodic() {
+   
+    left_motor.set(TalonFXControlMode.PercentOutput, motorPower);
+    right_motor.follow(left_motor);
+    // left_motor.follow(right_motor);
+    // right_motor.set(TalonFXControlMode.PercentOutput, motorPower);
+    // left_motor.follow(right_motor);
+
+    //left_motor.set(TalonFXControlMode.PercentOutput, motorPower);
+>>>>>>> main
   }
 
 
