6ce7a3021f30940afe8265d46b8ff90f5488c3f0
EvelynCoolMop <evelynhon17@gmail.com>
2023-10-26 16:03:10 -0700

    makes it obey commands????????

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ManualArmCommand;
+import frc.robot.commands.PositionArmCommand;
 import frc.robot.subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -45,7 +46,7 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
    
-
+    driverA.a().onTrue(new PositionArmCommand(elevatorSubsystem));
     
   }
 
