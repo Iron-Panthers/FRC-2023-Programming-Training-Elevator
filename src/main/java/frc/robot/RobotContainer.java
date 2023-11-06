098e1b6a181c4606ccbf40a8b6dc819c94418233
Samantha <27nguyens28@stu.smuhsd.org>
2023-11-06 15:27:45 -0800

    Changed the target height numbers
    rahh Samantha is a nice person

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,12 +48,12 @@ public class RobotContainer {
   private void configureButtonBindings() {
    
     //if this button is pressed
-    driverA.a().onTrue(new PositionArmCommand(elevatorSubsystem, 10));
+    driverA.a().onTrue(new PositionArmCommand(elevatorSubsystem, 15));
 
     
-    driverA.b().onTrue(new PositionArmCommand(elevatorSubsystem, 4));
-    driverA.x().onTrue(new PositionArmCommand(elevatorSubsystem, 2));
-    driverA.y().onTrue(new PositionArmCommand(elevatorSubsystem, 8));
+    driverA.b().onTrue(new PositionArmCommand(elevatorSubsystem, 10));
+    driverA.x().onTrue(new PositionArmCommand(elevatorSubsystem, 5));
+    driverA.y().onTrue(new PositionArmCommand(elevatorSubsystem, 0));
     //run this command, to set the target height to ___
     //Position Arm Command
 
